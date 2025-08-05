84dde84c6f5fb168d1069246e1f33af1bd560d3d
Jackson Coxson <jkcoxson@gmail.com>
2025-08-05 13:28:40 -0600

    Initial Commit

---
+++ LightningRouter/LightningRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightningRouterApp.swift
+//  LightningRouter
+//
+//  Created by Jackson Coxson on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LightningRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
